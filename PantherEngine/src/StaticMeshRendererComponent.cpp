1e30284d04c4ecd081f93446fa18bd1d7ed4b969
Jadela <jasper.de.laat@gmail.com>
2017-06-11 17:49:46 +0200

    Implemented constant buffer & recording logic in StaticMeshRendererComponent

--- PantherEngine/src/StaticMeshRendererComponent.cpp
+++ PantherEngine/src/StaticMeshRendererComponent.cpp
@@ -1,11 +1,46 @@
 #include "StaticMeshRendererComponent.h"
 
+#include "Buffer.h"
+#include "CommandList.h"
+#include "DescriptorHeap.h"
+#include "Material.h"
 #include "Mesh.h"
+
+using namespace DirectX;
 
 namespace Panther
 {
-	StaticMeshRendererComponent::StaticMeshRendererComponent(Mesh* a_Mesh)
-		: m_Mesh(a_Mesh)
+	StaticMeshRendererComponent::StaticMeshRendererComponent(Buffer& a_ObjectCBuffer, DescriptorHeap& a_CBHeap, Mesh* a_Mesh, Material* a_Material) :
+		m_ObjectCBuffer(a_ObjectCBuffer),
+		m_CBHeap(a_CBHeap),
+		m_Mesh(a_Mesh),
+		m_Material(a_Material)
 	{
+		m_ObjectCBElementSlot = m_ObjectCBuffer.GetSlot();
+		m_ObjectCBHeapSlot = m_CBHeap.RegisterConstantBuffer(m_ObjectCBuffer, m_ObjectCBElementSlot);
+	}
+
+	void StaticMeshRendererComponent::UpdateObjectCB(DirectX::XMMATRIX a_WorldMatrix, DirectX::XMMATRIX a_ViewProjectionMatrix)
+	{
+		struct ObjectCB
+		{
+			XMMATRIX m_MVP;
+			XMMATRIX m_M;
+			XMMATRIX m_IT_M;
+		};
+
+		ObjectCB objectCB;
+		objectCB.m_MVP = a_WorldMatrix * a_ViewProjectionMatrix;
+		objectCB.m_M = a_WorldMatrix;
+		objectCB.m_IT_M = XMMatrixTranspose(XMMatrixInverse(nullptr, a_WorldMatrix));
+		m_ObjectCBuffer.CopyTo(m_ObjectCBElementSlot, &objectCB, sizeof(ObjectCB));
+	}
+
+	void StaticMeshRendererComponent::Record(CommandList& a_CommandList)
+	{
+		m_Material->SetResource("ObjectCB", m_CBHeap, m_ObjectCBHeapSlot);
+		m_Material->Use(a_CommandList);
+		a_CommandList.SetMesh(*m_Mesh);
+		a_CommandList.Draw(m_Mesh->GetNumIndices());
 	}
 }
--- PantherEngine/src/StaticMeshRendererComponent.h
+++ PantherEngine/src/StaticMeshRendererComponent.h
@@ -1,16 +1,31 @@
 #pragma once
 #include "Component.h"
+#include "Core.h"
+
+#include <DirectXMath.h>
 
 namespace Panther
 {
+	class Buffer;
+	class CommandList;
+	class DescriptorHeap;
+	class Material;
 	class Mesh;
 
 	class StaticMeshRendererComponent final : public Component
 	{
 	public:
-		StaticMeshRendererComponent(Mesh* a_Mesh);
+		StaticMeshRendererComponent(Buffer& a_ObjectCBuffer, DescriptorHeap& a_CBHeap, Mesh* a_Mesh, Material* a_Material);
+
+		void UpdateObjectCB(DirectX::XMMATRIX a_WorldMatrix, DirectX::XMMATRIX a_ViewProjectionMatrix);
+		void Record(CommandList& a_CommandList);
 
 	private:
+		Buffer& m_ObjectCBuffer;
+		DescriptorHeap& m_CBHeap;
+		uint32 m_ObjectCBElementSlot;
+		uint32 m_ObjectCBHeapSlot;
 		Mesh* m_Mesh;
+		Material* m_Material;
 	};
 }
