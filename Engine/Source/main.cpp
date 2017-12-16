de78e2841452d170c8125faddc0251159a053687
Jadela <jasper.de.laat@live.nl>
2017-12-16 19:38:45 +0100

    Fixed DPI awareness.

--- Engine/Source/main.cpp
+++ Engine/Source/main.cpp
@@ -9,10 +9,15 @@ using namespace Panther;
 
 int WINAPI wWinMain(HINSTANCE a_InstanceHandle, HINSTANCE a_Nothing, PWSTR a_CommandLineArguments, int a_ShowCmd)
 {
-	// Enable run-time memory check for debug builds.
 #if CONFIG_DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+	// Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
+	// Using this awareness context allows the client area of the window 
+	// to achieve 100% scaling while still allowing non-client window content to 
+	// be rendered in a DPI sensitive fashion.
+	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
 	try
 	{
